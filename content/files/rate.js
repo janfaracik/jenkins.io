67dc70ef47aaeb6e447b81e46d669efa2c230aa8
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-10-07 21:44:23 +0100

    Init

--- content/files/rate.js
+++ content/files/rate.js
@@ -20,7 +20,7 @@ function health(nm,cls,ver,rate,desc) {
 
 function do_loaddata() {
   var r, v, j = true, div1, div2, txt;
-  for (var anchors = document.getElementsByTagName('H3'), i = 0; i < anchors.length; i++) {
+  for (var anchors = document.getElementsByTagName('H2'), i = 0; i < anchors.length; i++) {
     if (anchors[i].id.charAt(0) != 'v') continue;
     r = data[v = anchors[i].id.substring(1)];
     div1 = document.createElement('DIV');
@@ -30,17 +30,23 @@ function do_loaddata() {
     txt = (r && r[0] ? r[0] + ' ' : '0 ') + health('sunny',(r && r[0] ? '' : 'light'),v,1, 'No major issues with this release')
         + (r && r[1] ? r[1] + ' ' : '0 ') + health('cloudy',(r && r[1] ? '' : 'light'),v,0, 'I experienced notable issues')
         + (r && r[2] ? r[2] + ' ' : '0 ') + health('storm',(r && r[2] ? '' : 'light'),v,-1, 'I had to roll back');
+    div2.innerHTML = txt;
+    div1.appendChild(div2);
+
     if (r && r.length > 3) {
-      txt += '<span class="related-issues">Community reported issues: ';
+      const div3 = document.createElement('DIV');
+      div3.className = 'rate-text';
+      let communityReportedIssues = '<span class="related-issues">Community reported issues: ';
       var issues = [];
       for (j = 3; j < r.length; j += 2) {issues.push({id: r[j], count: r[j + 1]})}
       issues.sort(function (a, b) {return b.count - a.count;});
       for (j = 0; j < issues.length; j++)
-        txt += issues[j].count + '&times;<a href="https://issues.jenkins.io/browse/JENKINS-' + issues[j].id + '">JENKINS-' + issues[j].id + '</a> ';
-      txt += '</span>';
+        communityReportedIssues += issues[j].count + '&times;<a href="https://issues.jenkins.io/browse/JENKINS-' + issues[j].id + '">JENKINS-' + issues[j].id + '</a> ';
+      communityReportedIssues += '</span>';
+      div3.innerHTML = communityReportedIssues;
+      div1.appendChild(div3);
     }
-    div2.innerHTML = txt;
-    div1.appendChild(div2);
+
     anchors[i].parentNode.insertBefore(div1, anchors[i].nextElementSibling);
   }
 }
